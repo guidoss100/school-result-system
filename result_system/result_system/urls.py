6758b485e9168f4b9539010102049e91fe21f386
Amos Yeboah <guidossmultimedia@gmail.com>
2026-03-24 23:06:40 +0000

    fix urls structure

--- result_system/result_system/urls.py
+++ result_system/result_system/urls.py
@@ -4,8 +4,10 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', views.home, name='home'),
+
     path('class-results/<int:class_id>/<str:term>/', views.class_results, name='class_results'),
-    
-    path('', include('core.urls')),   # ← VERY IMPORTANT
+
+    path('', include('core.urls')),
 ]
