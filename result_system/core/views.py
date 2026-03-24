d93dcb21bd4e0ab588b6322c8176b544f09b8ab5
Amos Yeboah <guidossmultimedia@gmail.com>
2026-03-24 20:23:19 +0000

    added real homepage

--- result_system/core/views.py
+++ result_system/core/views.py
@@ -11,7 +11,7 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 
 def home(request):
-    return HttpResponse("🚀 School Result System is Live!")
+    return render(request, "core/home.html")
 
 def promote_students():
     
