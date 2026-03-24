7eeb049e15590117f3e172189314bdbe8ae6a016
Amos Yeboah <guidossmultimedia@gmail.com>
2026-03-24 22:58:36 +0000

    fix url order

--- result_system/result_system/urls.py
+++ result_system/result_system/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from core import views
 
 urlpatterns = [
-    path('class-results/<int:class_id>/<str:term>/', views.class_results, name='class_results'),
-    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-
+    path('', views.home, name='home'),
+    path('class-results/<int:class_id>/<str:term>/', views.class_results, name='class_results'),
+    
     path('', include('core.urls')),   # ← VERY IMPORTANT
 ]
