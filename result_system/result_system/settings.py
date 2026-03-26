3eb26635664951fdac877ad6e0511b12450fb78d
Amos Yeboah <guidossmultimedia@gmail.com>
2026-03-26 20:14:02 +0000

    fix python path for core app

--- result_system/result_system/settings.py
+++ result_system/result_system/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 import dj_database_url
+import sys
 import os
 from pathlib import Path
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
