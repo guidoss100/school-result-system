4f2d8bf1bc4c06419a460b93070d39fb9e4eb051
Amos Yeboah <guidossmultimedia@gmail.com>
2026-03-26 20:58:37 +0000

    fix settings module path

--- result_system/result_system/wsgi.py
+++ result_system/result_system/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'result_system.result_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'result_system.settings')
 
 application = get_wsgi_application()
